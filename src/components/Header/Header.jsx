cf8a1187d6278e59ed3bdf121a8f1b55a4d5e7c9
Novel Thunderstorm <novelthunderstorm@gmail.com>
2023-07-25 23:50:27 +0300

    refactoring nav/header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,42 @@
 import { useState } from "react";
-import logo from "./../../images/logo.svg"
-import { Link, useLocation } from 'react-router-dom';
+import logo from "./../../images/logo.svg";
+import { Link } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
-import './Header.css'
+import "./Header.css";
 function Header({ loggedIn }) {
-  const location = useLocation();
-
   const [isBurgerMenuOpen, setBurgerMenuOpen] = useState(false);
 
   const handleBurgerMenuToggle = () => {
-    setBurgerMenuOpen(prevState => !prevState);
+    setBurgerMenuOpen((prevState) => !prevState);
   };
 
   return (
-    <header className='header'>
+    <header className="header">
       <div className="header__container">
         <Link className="header__logo-link" to="/">
           <img className="header__logo" src={logo} alt="logotype" />
         </Link>
-        {loggedIn
-          ?
+        {loggedIn ? (
           <>
-            <button className={`navigation__menu`} onClick={handleBurgerMenuToggle}></button>
-            <Navigation isBurgerMenuOpen={isBurgerMenuOpen} setBurgerMenuOpen={setBurgerMenuOpen} handleBurgerMenuToggle={handleBurgerMenuToggle} />
+            <button
+              className={`navigation__menu`}
+              onClick={handleBurgerMenuToggle}
+            ></button>
+            <Navigation
+              isBurgerMenuOpen={isBurgerMenuOpen}
+              handleBurgerMenuToggle={handleBurgerMenuToggle}
+            />
           </>
-          :
+        ) : (
           <nav className="header__auth-container">
-            <Link to="/signup" className="header__link link-hover">Регистрация</Link>
-            <Link to="/signin" className="header__button button-hover">Войти</Link>
+            <Link to="/signup" className="header__link link-hover">
+              Регистрация
+            </Link>
+            <Link to="/signin" className="header__button button-hover">
+              Войти
+            </Link>
           </nav>
-        }
+        )}
       </div>
     </header>
   );
